098df7f01672159e9d4e8a1c71b60d7fed0609de
Charles <charleslcsantos@gmail.com>
2021-10-15 01:04:13 -0300

    added firebase integration test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
+import { collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAG7SiJpRPyCuNKOnC3MWh3bsxrjF3MkX8',
+  authDomain: 'meuponto-22c8a.firebaseapp.com',
+  databaseURL: 'https://meuponto-22c8a.firebaseio.com',
+  projectId: 'meuponto-22c8a',
+  storageBucket: 'meuponto-22c8a.appspot.com',
+  messagingSenderId: '453391659544',
+  appId: '1:453391659544:web:ffcce5de0317526828c96d',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'meuponto';
+
+  async ngOnInit(): Promise<void> {
+    const querySnapshot = await getDocs(collection(db, 'registros'));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, doc.data());
+    });
+  }
 }
